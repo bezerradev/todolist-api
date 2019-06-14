933d51c92469f7614ba55618a7a2cc37d2d795a9
Filipe Viana Monteiro <filipe.monteiro@imd.local>
2019-06-14 14:14:01 -0300

    commit inicial

---
+++ index.js
@@ -0,0 +1,24 @@
+    
+var express = require('express');
+var app = express();
+var bodyParser = require('body-parser');
+// var fs = require('fs')
+// var path = require('path');
+// var cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+require('dotenv').config()
+app.set('port', (process.env.PORT || 5000));
+app.set('views', __dirname + '/public');
+
+app.get('/', function(req, res) {
+	res.json({ mensagem: 'funfou!' });
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app running at localhost:" + app.get('port'));
+});
+
+module.exports = app;

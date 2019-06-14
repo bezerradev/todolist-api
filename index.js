7cdf1a16fd3b1d96964731a8f1f649927c4b5c62
Mateus Bezerra <bezerra.dev@gmail.com>
2019-06-14 14:25:55 -0300

    soh vai

--- index.js
+++ index.js
@@ -2,18 +2,30 @@
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
-// var fs = require('fs')
-// var path = require('path');
-// var cors = require('cors');
+var fs = require('fs')
+var path = require('path');
+var cors = require('cors');
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.use(cors());
+
+var idAtualProjeto = 1;
+var idAtualTarefa = 1;
 
 require('dotenv').config()
 app.set('port', (process.env.PORT || 5000));
 app.set('views', __dirname + '/public');
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/main.html'));
+});
+
+app.get('/tarefas', function(req, res) {
+	res.json({ mensagem: 'funcionou!' });
+});
+
+app.post('/tarefas', function(req, res) {
 	res.json({ mensagem: 'funcionou!' });
 });
 
